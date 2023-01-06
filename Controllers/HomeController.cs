837daeebaa12451ff551f438615e708527d0c8c1
Bhaskar Singh <kitchauhan@gmail.com>
2023-01-05 20:02:57 -0800

    Code updated for static contents.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,7 +19,13 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View();
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestResponse> _response = new List<GuestResponse>();
+        public static IEnumerable<GuestResponse> Responses => _response;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            _response.Add(response);
+        }
+    }
+}
